c70f1690e634ed6d4211fecf5fa440a613756b92
eduardagoulart <goularteduarda.a@gmail.com>
2019-04-18 13:59:39 -0300

    normalizando a quantidade de episodios

--- pre_processing.py
+++ pre_processing.py
@@ -5,12 +5,26 @@ class DataProcessing:
     def __init__(self):
         self.file = pd.read_csv("anime.csv")
 
+
     def anime_type(self):
         types = self.file["type"]
         types = [[1 if referential == video_type else 0 for referential in types] for video_type in types]
-        # print(types)
         return types
+
+    def ep(self):
+        episodes = self.file["episodes"]
+        for i in range(0, len(episodes)):
+            try:
+                episodes[i] = int(episodes[i])
+            except:
+                episodes[i] = 0
+        standard = episodes.copy()
+        standard = standard.tolist()
+        standard.sort(reverse=True)
+        max_value = standard[0]
+        standard = [value / max_value for value in episodes]
+        return standard
 
 
 if "__main__" == __name__:
-    DataProcessing().anime_type()
+    DataProcessing().ep()
