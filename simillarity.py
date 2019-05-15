a558a14be880753cd6b7d8777bf36400425c2899
eduardagoulart <goularteduarda.a@gmail.com>
2019-05-15 17:29:39 -0300

    similaridade entre quantidade eps, membros e nota"

--- simillarity.py
+++ simillarity.py
@@ -1,21 +1,28 @@
 from pre_processing import DataProcessing
 import sys
 
+obj = DataProcessing()
+type_animes = obj.anime_type()
+qt_ep = obj.ep()
+grades = obj.grades()
+num_members = obj.members()
+gender = obj.normalize_gender().copy()
+
+
+def ep_rating_member():
+    sum_values = [qt_ep[i] + grades[i] + num_members[i] for i in range(0, len(num_members))]
+    max_value = max(sum_values)
+    return [i / max_value for i in sum_values]
+
 
 def all_values(ref_id):
     obj = DataProcessing()
     type_animes = obj.anime_type()
     qt_ep = obj.ep()
     grades = obj.grades()
-    print(grades)
-    # num_members = obj.members().copy()
-    # gender = obj.normalize_gender().copy()
+    num_members = obj.members()
+    gender = obj.normalize_gender().copy()
 
 
 if __name__ == '__main__':
-    try:
-        id_video = sys.argv[1]
-        all_values(id_video)
-    except:
-        print("Argumento inválido para começar a playlist")
-        exit(404)
+    ep_rating_member()
