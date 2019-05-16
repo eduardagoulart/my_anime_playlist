64a1b1a3fb03558417db88db05c800c7669d984c
eduardagoulart <goularteduarda.a@gmail.com>
2019-05-16 10:15:53 -0300

    criando ligações de similaridade usando dados de rating, ep e membros

--- clusters.py
+++ clusters.py
@@ -1,11 +1,20 @@
 import simillarity
 
 
-def ep_rating_member_cluster():
+def ep_rating_member_id():
     id_anime = simillarity.obj.id_anime()
     simillarity_list = simillarity.ep_rating_member()
-    return [[id_anime[i], simillarity_list[i]] for i in range(0, len(simillarity_list))]
+    return [[int(id_anime[i]), simillarity_list[i]] for i in range(0, len(simillarity_list))]
+
+
+def trace_ep_rating_member():
+    simillarity_list = ep_rating_member_id()
+    final_cluster = [[ad, sim] for ad, sim in simillarity_list if 0.6 <= sim <= 1.0]
+    print(final_cluster)
+    for i in simillarity_list:
+        if i[0] == 24:
+            print(f'24: {i}')
 
 
 if __name__ == '__main__':
-    ep_rating_member_cluster()
+    trace_ep_rating_member()
