3a8c8b5a3e91ebca22926576053e59cc60b8e988
eduardagoulart <goularteduarda.a@gmail.com>
2019-05-23 08:35:38 -0300

    tentando separar as notas dos usuários

--- rating_separate.py
+++ rating_separate.py
@@ -1,9 +1,21 @@
 import pandas as pd
+import _pickle as pickle
 
-df = pd.read_csv('rating.csv')
+
+data = pd.read_csv('rating.csv')
 anime_id = []
-for i in df['user_id']:
+for i in data['user_id']:
     if i not in anime_id:
         anime_id.append(i)
 
-print(anime_id)
+df = pd.DataFrame(data, index=['user_id', 'anime_id', 'rating'])
+
+df_filter = {}
+for i in anime_id:
+    df_filter[i] = [(df.user_id == i)]
+
+print(df_filter)
+
+with open('file.txt', 'w') as file:
+    file.write(pickle.dumps(df_filter))
+
