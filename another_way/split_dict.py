db8d865df80d1f55dd531294578747e64abc85be
eduardagoulart <goularteduarda.a@gmail.com>
2019-05-31 15:20:28 -0300

    tentado adicionar os dicionarios com valores

--- another_way/split_dict.py
+++ another_way/split_dict.py
@@ -1,18 +1,40 @@
 import csv
+from pandas import read_csv
 
 
-def read_file(file):
-    user = {}
-    with open(file, newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+def read(file):
+    # spamreader = open('rating.csv', 'r')
+    # spamreader = csv.reader(spamreader, )
+    # for row in spamreader:
+    #     print(row)
+    # spamreader = read_csv(file)
+    # for i in spamreader['user_id']:
+    #    print(i)
+
+    with open(file) as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
         # user[spamreader[0]] = {spamreader[1]: spamreader[2]}
-        data_user = [row for row in spamreader]
-        for i in data_user:
-            value = i[0].split(',')
-            if len(value) == 3:
+        # data_user = [row for row in spamreader]
+        for row in spamreader:
+            print(row)
+        # print(data_user)
+        # print(1044 in data_user)
+    '''
+
+    user = {}
+    for i in data_user:
+        print(i)
+
+        value = i[0].split(',')
+        if len(value) == 3:
+            if value[0] in user.keys():
+                user[value[0]].update({value[1]: value[2]})
+            else:
                 user[value[0]] = {value[1]: value[2]}
+                '''
 
-    print(user)
+
+                # print(user.keys())
 
 
-read_file('rating.csv')
+read('rating.csv')
