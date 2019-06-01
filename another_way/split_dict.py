fd6a1f352229f0459f87bdd3490d4151cd33c3f2
eduardagoulart <goularteduarda.a@gmail.com>
2019-06-01 12:05:50 -0300

    separando os videos assistidos por usuario

--- another_way/split_dict.py
+++ another_way/split_dict.py
@@ -14,27 +14,24 @@ def read(file):
     with open(file) as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',')
         # user[spamreader[0]] = {spamreader[1]: spamreader[2]}
-        # data_user = [row for row in spamreader]
-        for row in spamreader:
-            print(row)
+        data_user = [row for row in spamreader]
+
         # print(data_user)
         # print(1044 in data_user)
-    '''
 
     user = {}
     for i in data_user:
-        print(i)
+        
 
-        value = i[0].split(',')
-        if len(value) == 3:
-            if value[0] in user.keys():
-                user[value[0]].update({value[1]: value[2]})
-            else:
-                user[value[0]] = {value[1]: value[2]}
-                '''
+        # value = i[0].split(',')
 
-
-                # print(user.keys())
+        if i[0] in user.keys():
+            user[i[0]].update({i[1]: i[2]})
+            print('usuário já existe')
+        else:
+            user[i[0]] = {i[1]: i[2]}
+            print('novo usuário')
+    print(f'opa: {user}')
 
 
 read('rating.csv')
