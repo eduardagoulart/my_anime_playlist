b36547d4986ec9a09c75e572952fd1223aa7e24a
eduardagoulart <goularteduarda.a@gmail.com>
2019-06-03 08:29:43 -0300

    calculo de pearson

--- another_way/sim_pearson.py
+++ another_way/sim_pearson.py
@@ -1,12 +1,31 @@
-from split_dict import read
 from math import sqrt
 
 
 def sim(users, p1, p2):
+    # p1, p2 = int(p1), int(p2)
+    print(type(p1))
+    print(users[p1])
     si = {}
     for item in users[p1]:
         if item in users[p2]:
             si[item] = 1
 
     qtd_sim = len(si)
-    print(qtd_sim)
+
+    if qtd_sim == 0:
+        return 0
+
+    sum1 = sum([users[p1][item] for item in si])
+    sum2 = sum([users[p2][item] for item in si])
+
+    squares_sum1 = sum([pow(users[p1][item], 2) for item in si])
+    squares_sum2 = sum([pow(users[p2][item], 2) for item in si])
+
+    product_sum = sum([users[p1][item] * users[p2][item] for item in si])
+
+    num = product_sum - (sum1 * sum2 / qtd_sim)
+    den = sqrt((squares_sum1 - pow(sum1, 2) / qtd_sim) * (squares_sum2 - pow(sum2, 2) / qtd_sim))
+    if den == 0:
+        return 0
+
+    return num / den
--- another_way/split_dict.py
+++ another_way/split_dict.py
@@ -1,8 +1,8 @@
 import csv
 
 
-def read(file='rating.csv'):
-    with open(file) as csvfile:
+def read():
+    with open('another_way/rating.csv') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',')
         data_user = [row for row in spamreader]
 
@@ -11,8 +11,6 @@ def read(file='rating.csv'):
 
         if i[0] in user.keys():
             user[i[0]].update({i[1]: i[2]})
-            print('usuário já existe')
         else:
             user[i[0]] = {i[1]: i[2]}
-            print('novo usuário')
     return user
