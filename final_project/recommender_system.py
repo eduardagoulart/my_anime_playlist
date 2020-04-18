fa3f3743833ff20ae414b5367c37002e388bea6a
eduardagoulart <goularteduarda.a@gmail.com>
2020-04-18 19:23:56 -0300

    removing comments

--- final_project/recommender_system.py
+++ final_project/recommender_system.py
@@ -1,22 +1,15 @@
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-# from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
 def compare_gender(df):
-    # count_vect = CountVectorizer()
-    import pudb; pudb.set_trace()
     vectorizer = TfidfVectorizer()
     corpus = [df.genre[1], df.genre[2]]
     print(corpus)
     trsfm=vectorizer.fit_transform(corpus)
-    # print(trsfm)
-    # df = pd.DataFrame(trsfm.toarray(),columns=vectorizer.get_feature_names())
-    # print(df)
-    # return cosine_similarity(df)
     print(cosine_similarity(trsfm[0:1], trsfm))
 
 
@@ -25,11 +18,6 @@ if __name__ == "__main__":
     df["type"] = df["type"].replace("nan", np.nan)
     df["genre"] = df["genre"].replace(" ", "")
     print(len(df))
-    # try:
-    #     df["genre"] = df["genre"].replace(" ", "").str.split(',')
-    # except:
-    #     pass
     df["episodes"] = df["episodes"].replace("Unknown", np.nan)
     df = df.dropna(subset=["type", "episodes", "rating", "members"])
     aux = compare_gender(df[["anime_id", "genre"]])
-    # print(compare_gender(df[["anime_id", "genre"]]))
